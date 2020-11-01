963a002d2939a41b84d3a3e7881cf0d131753e46
nmarzz <ndmarshall97@gmail.com>
2020-11-01 15:01:43 -0500

    animate behind navigation

--- animate.js
+++ animate.js
@@ -303,13 +303,13 @@ function draw()
 {
 
   var info = document.getElementById("info").getBoundingClientRect();
-  
+
 
   context.clearRect(0, 0, myCanvas.width, myCanvas.height)
   kmeans(pc.points)
   plotpoints(pc.points,context)
   drawClusters(pc.points,context)
-  context.clearRect(info.top, info.left,info.width,info.height)
+  //context.clearRect(info.top, info.left,info.width,info.height)
 
 
   if (document.getElementById("animateSwitch").checked){
